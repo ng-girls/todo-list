d28527ad10f586dead1c5ad363f9ebe92b2d14b5
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2018-08-06 12:32:11 +0300

    feat(tests): new e2e tests for presentation example

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,12 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, ElementArrayFinder, ElementFinder, $, $$ } from 'protractor';
 
 export class TodoListPage {
+
+  todosArray: ElementArrayFinder = $$('li todo-item');
+  newTodoInput: ElementFinder = $('.todo-add todo-input input');
+  newTodoSaveBtn: ElementFinder = $('.todo-add todo-input button');
+  removeBtnsArray: ElementArrayFinder = this.todosArray.$$('.btn.btn-red');
+
   navigateTo() {
     return browser.get('/');
   }
